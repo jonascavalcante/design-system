04efb56cebb5b49ccd2392536e8406a80d6d37da
Jonas Cavalcante <jonascavalcantejc@gmail.com>
2022-10-14 12:08:37 -0300

    Create layout

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,22 +1,30 @@
-import { ReactNode } from "react";
+import { ButtonHTMLAttributes, ReactNode } from "react";
 import { Slot } from "@radix-ui/react-slot";
 
 import { clsx } from "clsx";
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   asChild?: boolean;
+  className?: string;
 }
 
-export function Button({ children, asChild }: ButtonProps) {
+export function Button({
+  children,
+  asChild,
+  className,
+  ...props
+}: ButtonProps) {
   const Comp = asChild ? Slot : "button";
 
   return (
     <Comp
       className={clsx(
         "py-3 px-4 rounded font-semibold text-black text-md w-full transition-colors bg-cyan-500 hover:bg-cyan-300 focus:ring-2 ring-white",
-        {}
+        {},
+        className
       )}
+      {...props}
     >
       {children}
     </Comp>
