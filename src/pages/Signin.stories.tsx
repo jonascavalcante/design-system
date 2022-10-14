d8e8aaa2172bf163672eae873220bcbb035c697c
Jonas Cavalcante <jonascavalcantejc@gmail.com>
2022-10-14 16:52:12 -0300

    Add tests

---
+++ src/pages/Signin.stories.tsx
@@ -0,0 +1,31 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { userEvent, waitFor, within } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
+
+import { Signin } from "./Signin";
+
+export default {
+  title: "Pages/Sign in",
+  component: Signin,
+  args: {},
+  argTypes: {},
+} as Meta;
+
+export const Default: StoryObj = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    userEvent.type(
+      canvas.getByPlaceholderText("Digite seu e-mail"),
+      "jonas@mail.com"
+    );
+
+    userEvent.type(canvas.getByPlaceholderText("********"), "jonas123");
+
+    userEvent.click(canvas.getByRole("button"));
+
+    await waitFor(() => {
+      return expect(canvas.getByText("Login realizado!")).toBeInTheDocument();
+    });
+  },
+};
